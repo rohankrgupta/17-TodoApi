a35dda44536805a9ee4351e1a889ff1114092fa3
Rohan Gupta <rohankrgupta2000@gmail.com>
2024-05-26 20:27:58 +0530

    "Setup Patch method for url params"

--- src/main/java/com/example/todoapi/TodoController.java
+++ src/main/java/com/example/todoapi/TodoController.java
@@ -17,7 +17,7 @@ public class TodoController {
     // in-server memory, server wiped then data is lost
     private static List<Todo> todoList;
 
-    public TodoController(){
+    public TodoController() {
         todoList = new ArrayList<>();
         todoList.add(new Todo(1, false, "todo-1", 1));
         todoList.add(new Todo(2, true, "todo-2", 2));
@@ -35,26 +35,71 @@ public class TodoController {
 //        return newTodo;
 //    }
 
+    private int fetchTodoIndex(Long todoId) {
+        int index = -1;
+
+        for (int i = 0; i < todoList.size(); i++) {
+            if (todoList.get(i).getId() == todoId)
+                index = i;
+        }
+        return index;
+    }
+
     @GetMapping
-    public ResponseEntity<List<Todo>>getTodos(){
+    public ResponseEntity<List<Todo>> getTodos() {
         return ResponseEntity.status(HttpStatus.OK).body(todoList);
     }
 
     @PostMapping
-    public ResponseEntity<Todo> createTodos(@RequestBody Todo newTodo){
+    public ResponseEntity<Todo> createTodos(@RequestBody Todo newTodo) {
         todoList.add(newTodo);
         return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
     }
 
     // specifying url params
     @GetMapping("/{todoId}")
-    public ResponseEntity<?>getTodoById(@PathVariable Long todoId){
-        for(Todo todo : todoList){
-            if(todo.getId() == todoId){
+    public ResponseEntity<?> getTodoById(@PathVariable Long todoId) {
+        for (Todo todo : todoList) {
+            if (todo.getId() == todoId) {
                 return ResponseEntity.status(HttpStatus.OK).body(todo);
             }
         }
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(TODO_NOT_FOUND);
+    }
+
+    @DeleteMapping("/{todoId}")
+    public ResponseEntity<String> deleteTodoById(@PathVariable Long todoId) {
+        int index = fetchTodoIndex(todoId);
+        if (index == -1) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(TODO_NOT_FOUND);
+        } else {
+            todoList.remove(index);
+            return ResponseEntity.status(HttpStatus.OK).body("Deleted successfully");
+        }
+    }
+
+    @PatchMapping("/{todoId}")
+    public ResponseEntity<?> updateTodoById(
+            @PathVariable Long todoId,
+            @RequestParam(required = false) Integer id,
+            @RequestParam(required = false) Boolean completed,
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) Integer userId)
+    {
+        int index = fetchTodoIndex(todoId);
+
+        if(index == -1){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(TODO_NOT_FOUND);
+        }
+
+        Todo referredTodo = todoList.get(index);
+
+        if(id != null) referredTodo.setId(id);
+        if(completed != null) referredTodo.setCompleted(completed);
+        if(title != null) referredTodo.setTitle(title);
+        if(userId != null) referredTodo.setUserId(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(referredTodo);
     }
 }
